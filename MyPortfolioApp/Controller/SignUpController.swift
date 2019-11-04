aa8dd22422b4fb70e752cd3f425f361e55ec9fa7
Vadim Khablov <vadim.khablov@yahoo.de>
2019-11-04 14:07:23 +0100

    Connect the database with the project

---
+++ MyPortfolioApp/Controller/SignUpController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpController.swift
+//  MyPortfolioApp
+//
+//  Created by Vadim Khablov on 04.11.19.
+//  Copyright © 2019 Vadim Khablov. All rights reserved.
+//
+
+import Foundation
---
+++ MyPortfolioApp/DesignHelper/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  MyPortfolioApp
+//
+//  Created by Vadim Khablov on 04.11.19.
+//  Copyright © 2019 Vadim Khablov. All rights reserved.
+//
+
+import Foundation
